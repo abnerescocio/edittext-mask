8cdb35b6e729ee3bdecebec5b90a9b1cf8371098
abnerescocio <abner.escocio@gmail.com>
2018-05-28 11:19:40 -0300

    Mask to phone number work fine

--- lib/src/main/java/com/abnerescocio/lib/TextInputEditTextMask.kt
+++ lib/src/main/java/com/abnerescocio/lib/TextInputEditTextMask.kt
@@ -5,12 +5,11 @@ import android.content.res.TypedArray
 import android.graphics.Rect
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
-import android.text.Editable
-import android.text.TextWatcher
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.util.AttributeSet
 
 class TextInputEditTextMask(context: Context?, attributeSet: AttributeSet?)
-    : TextInputEditText(context, attributeSet), TextWatcher {
+    : TextInputEditText(context, attributeSet) {
 
     private var defStyleAttr: Int = 0
     private var typeArray: TypedArray? = null
@@ -27,6 +26,7 @@ class TextInputEditTextMask(context: Context?, attributeSet: AttributeSet?)
                 R.styleable.TextInputEditTextMask, 0, 0)
         maskIdentifer = typeArray?.getInt(R.styleable.TextInputEditTextMask_mask, 0)
         mask = MASK.valueOf(maskIdentifer)
+        if (mask == MASK.PHONE) addTextChangedListener(PhoneNumberFormattingTextWatcher())
         typeArray?.recycle()
     }
 
@@ -35,29 +35,28 @@ class TextInputEditTextMask(context: Context?, attributeSet: AttributeSet?)
         if (focused) hint = mask?.getHint()
     }
 
-    override fun afterTextChanged(s: Editable?) {
-
-    }
-
     override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
         val inputLayout = parent?.parent
         if (inputLayout is TextInputLayout) {
-            if (mask != null && text?.length?.compareTo(0) == 1) {
+            if (mask != null && text?.length?.compareTo(LENGTH_ZERO) == GREATER_THAN) {
                 mask?.getRegex()?.matches(text)?.let {
                     if (it) inputLayout.error = null
                     else inputLayout.error = context.getString(mask?.getStringResIdToNoMatch()!!)
                 }
-            } else if (text?.length?.compareTo(0) == 0) inputLayout.error = null
+            } else if (text?.length?.compareTo(LENGTH_ZERO) == EQUALS_THAN) inputLayout.error = null
         }
     }
 
-    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-    }
-
     companion object {
+        private const val TAG = "edittext-mask"
+
+        private const val LENGTH_ZERO = 0
+        private const val GREATER_THAN = 1
+        private const val EQUALS_THAN = 1
+
         const val EMAIL = 100
         const val EMAIL_GMAIL = 101
+        const val PHONE = 200
         const val BRAZILIAN_PHONE_NUMBER = 1001
         const val BRAZILIAN_RG = 1002
         const val BRAZILIAN_CPF = 1003
