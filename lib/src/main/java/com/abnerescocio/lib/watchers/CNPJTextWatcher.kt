a2ef6bdbd73f172b375c7252ce627b245ae57e15
abnerescocio <abner.escocio@gmail.com>
2018-09-06 16:48:45 -0300

    Watcher to CNPJ

---
+++ lib/src/main/java/com/abnerescocio/lib/watchers/CNPJTextWatcher.kt
@@ -0,0 +1,20 @@
+package com.abnerescocio.lib.watchers
+
+import android.text.Editable
+import android.widget.TextView
+
+class CNPJTextWatcher(view: TextView) : AppTextWatcher(view) {
+    override fun onChangeEditable(editable: Editable?, isCleaning: Boolean) {
+        if (!isCleaning) {
+            if (editable?.length == 2) editable.insert(2, ".")
+            if (editable?.length == 6) editable.insert(6, ".")
+            if (editable?.length == 10) editable.insert(10, "/")
+            if (editable?.length == 15) editable.insert(15, "-")
+        }
+    }
+
+    fun isValidCPF(): Boolean {
+
+        return false
+    }
+}
