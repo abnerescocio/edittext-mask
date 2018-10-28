cf6cd4ea12bfb97547696c0ed60b6d286ef5f61a
Mikeias <mikeias66@gmail.com>
2018-10-28 01:14:16 -0300

    CNPJ watcher fixed

--- lib/src/main/java/com/abnerescocio/lib/watchers/CNPJTextWatcher.kt
+++ lib/src/main/java/com/abnerescocio/lib/watchers/CNPJTextWatcher.kt
@@ -6,10 +6,10 @@ import android.widget.TextView
 class CNPJTextWatcher(view: TextView) : AppTextWatcher(view) {
     override fun onChangeEditable(editable: Editable?, isCleaning: Boolean) {
         if (!isCleaning) {
-            if (editable?.length == 2) editable.insert(2, ".")
-            if (editable?.length == 6) editable.insert(6, ".")
-            if (editable?.length == 10) editable.insert(10, "/")
-            if (editable?.length == 15) editable.insert(15, "-")
+            if (editable?.length == 3) editable.insert(2, ".")
+            if (editable?.length == 7) editable.insert(6, ".")
+            if (editable?.length == 11) editable.insert(10, "/")
+            if (editable?.length == 16) editable.insert(15, "-")
         }
     }
 }
