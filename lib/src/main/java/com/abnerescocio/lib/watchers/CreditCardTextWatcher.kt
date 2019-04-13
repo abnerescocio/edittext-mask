a57594f8f6d290278e1e4067e07189504509f173
abnerescocio <abner.escocio@gmail.com>
2019-04-12 23:21:03 -0300

    Full implementation of credit card

--- lib/src/main/java/com/abnerescocio/lib/watchers/CreditCardTextWatcher.kt
+++ lib/src/main/java/com/abnerescocio/lib/watchers/CreditCardTextWatcher.kt
@@ -5,6 +5,9 @@ import android.widget.TextView
 import com.abnerescocio.lib.R
 
 class CreditCardTextWatcher(private val view: TextView) : AppTextWatcher(view) {
+    
+    private val poolOfIcons = hashMapOf<Int, Int?>()
+    private var currentIndexIntoPool = 0
 
     override fun onChangeEditable(editable: Editable?, isCleaning: Boolean) {
         if (!isCleaning) {
@@ -16,44 +19,76 @@ class CreditCardTextWatcher(private val view: TextView) : AppTextWatcher(view) {
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         super.onTextChanged(s, start, before, count)
-        s?.toString()?.replace(" ", "")?.let { strCardNumber ->
-            when (strCardNumber.length) {
-                1 -> {
-                    when (strCardNumber.toInt()) {
-                        4 -> {
-                            setDrawable(R.drawable.ic_visa)
-                        }
+        val strCardNumber = s?.toString()?.replace(" ", "") ?: return
+        when (strCardNumber.length) {
+            INDEX1 -> {
+                when (strCardNumber.toInt()) {
+                    4 -> {
+                        poolOfIcons[INDEX1] = R.drawable.ic_visa
                     }
+                    else -> poolOfIcons[INDEX1] = null
                 }
-                2 -> {
-                    when (strCardNumber.toInt()) {
-                        38, 60 -> {
-                            setDrawable(R.drawable.ic_hipercard)
-                        }
-                        in 23..26, in 51..55 -> {
-                            setDrawable(R.drawable.ic_mastercard)
-                        }
-                    }
-                }
-                3 -> {
-                    when (strCardNumber.toInt()) {
-                        in 223..229, 270, 271 -> {
-                            setDrawable(R.drawable.ic_mastercard)
-                        }
-                    }
-                }
-                4 -> {
-                    when (strCardNumber.toInt()) {
-                        in 2221..2229, 2720 -> {
-                            setDrawable(R.drawable.ic_mastercard)
-                        }
-                    }
-                }
+                setDrawableByIndexOfPool(INDEX1)
             }
+            INDEX2 -> {
+                when (strCardNumber.toInt()) {
+                    38, 60 -> {
+                        poolOfIcons[INDEX2] = R.drawable.ic_hipercard
+                    }
+                    in 23..26, in 51..55 -> {
+                        poolOfIcons[INDEX2] = R.drawable.ic_mastercard
+                    }
+                    else -> poolOfIcons[INDEX2] = null
+                }
+                setDrawableByIndexOfPool(INDEX2)
+            }
+            INDEX3 -> {
+                when (strCardNumber.toInt()) {
+                    in 223..229, 270, 271 -> {
+                        poolOfIcons[INDEX3] = R.drawable.ic_mastercard
+                    }
+                    else -> poolOfIcons[INDEX3] = null
+                }
+                setDrawableByIndexOfPool(INDEX3)
+            }
+            INDEX4 -> {
+                when (strCardNumber.toInt()) {
+                    in 2221..2229, 2720 -> {
+                        poolOfIcons[INDEX4] = R.drawable.ic_mastercard
+                    }
+                    else -> poolOfIcons[INDEX4] = null
+                }
+                setDrawableByIndexOfPool(INDEX4)
+            }
+            else -> setDrawableByIndexOfPool(0)
         }
     }
 
-    private fun setDrawable(iconId: Int) {
-        view.setCompoundDrawablesWithIntrinsicBounds(0, 0, iconId, 0)
+    private fun setDrawableByIndexOfPool(indexOfPool: Int) {
+        val itemId = poolOfIcons[indexOfPool]
+        if (itemId != null) setDrawable(itemId)
+        else {
+            if (indexOfPool >= currentIndexIntoPool) {
+                indexOfPool.minus(1).downTo(INDEX1).forEach { index ->
+                    val lastItemId = poolOfIcons[index]
+                    if (lastItemId != null) {
+                        setDrawable(lastItemId)
+                        return
+                    } else setDrawable(0)
+                }
+            } else setDrawable(0)
+        }
+        currentIndexIntoPool = indexOfPool
+    }
+    
+    private fun setDrawable(itemId: Int) {
+        view.setCompoundDrawablesWithIntrinsicBounds(0, 0, itemId, 0)
+    }
+    
+    companion object {
+        private const val INDEX1 = 1
+        private const val INDEX2 = 2
+        private const val INDEX3 = 3
+        private const val INDEX4 = 4
     }
 }
